a7a9f7d78d00d04e5260fa685430724ba94f15b5
wz <wz4990@126.com>
2017-02-28 09:07:31 +0800

    create

---
+++ src/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("123");
+		System.out.println("aaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaa");
+		
+	}
+
+}
